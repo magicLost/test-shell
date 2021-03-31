49ca31ce326bf75edab126682152bb62ffcfcfe2
magicLost <oooze@mail.ru>
2021-03-31 14:19:15 +0300

    Auto commit | 3/31/2021, 2:19:15 PM

--- src/utils/Shell/index.js
+++ src/utils/Shell/index.js
@@ -1,5 +1,6 @@
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 const { promisify } = require("util");
+const wait = require("waait");
 
 const shellExec = promisify(exec);
 
@@ -11,21 +12,59 @@ const gitAdd = async () => {
   if (stderr) throw new Error(stderr);
 };
 
-const gitCommit = async () => {
-  const { stdout, stderr } = await shellExec(`git commit -m "Initial commit"`);
+const gitCommit = async (commitText) => {
+  const text = commitText
+    ? commitText
+    : `Auto commit | ${new Date().toLocaleString()}`;
+
+  const { stdout, stderr } = await shellExec(`git commit -m "${text}"`);
 
   console.log("stdout", stdout);
 
   if (stderr) throw new Error(stderr);
+};
+
+const gitStatus = async () => {
+  const { stdout, stderr } = await shellExec(`git status"`);
+
+  //console.log("stdout", stdout);
+
+  if (stderr) throw new Error(stderr);
+
+  return stdout;
 };
 
 const gitPush = async () => {
-  const { stdout, stderr } = await shellExec(`git push -u origin"`);
+  const { stdout, stderr } = await shellExec(`git push -u origin master`);
 
   console.log("stdout", stdout);
 
   if (stderr) throw new Error(stderr);
 };
+
+/* const gitPushWithSpawn = async () => {
+  const childProcess = spawn(`git`, ["push", "-u", "origin", "master"]);
+
+  childProcess.stdout.on("data", (data) => {
+    console.log("DATA", data.toString());
+    //proc.stdin.write();
+  });
+
+  childProcess.on("message", (msg) => {
+    console.log("MESSAGE", msg.toString());
+    //proc.stdin.write();
+  });
+
+  childProcess.on("exit", (data) => {
+    console.log("EXIT", data.toString());
+    //proc.stdin.write();
+  });
+
+  childProcess.on("disconnect", (data) => {
+    console.log("DISCONNECT", data.toString());
+    //proc.stdin.write();
+  });
+}; */
 
 const checkCommitedFiles = (stdout) => {
   const res = stdout.match(/.jpeg|.svg|.png|.jpg/);
@@ -35,9 +74,13 @@ const checkCommitedFiles = (stdout) => {
 const makeCommit = async () => {
   await gitAdd();
 
+  const filesToCommit = await gitStatus();
+
+  checkCommitedFiles(filesToCommit);
+
   await gitCommit();
 
   await gitPush();
 };
 
-gitAdd();
+makeCommit();
